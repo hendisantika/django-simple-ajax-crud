4ff4fb5a152a8ee6137131cc7168c995a6151a6c
hendisantika <hendisantika@yahoo.co.id>
2019-02-17 20:15:05 +0700

    #8 - Modify urls.py file

--- django_simple_ajax_crud/urls.py
+++ django_simple_ajax_crud/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+
+from books import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^books/$', views.book_list, name='book_list'),
 ]

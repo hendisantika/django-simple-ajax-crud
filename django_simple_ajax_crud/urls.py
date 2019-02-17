2043820540453f2cde13638e6b2270c41bcd0b90
hendisantika <hendisantika@yahoo.co.id>
2019-02-17 20:21:06 +0700

    #12 - Modify urls.py file

--- django_simple_ajax_crud/urls.py
+++ django_simple_ajax_crud/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from books import views
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^books/$', views.book_list, name='book_list'),
+    # url(r'^books/create/$', views.book_create, name='book_create'),
+    # url(r'^books/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
+    # url(r'^books/(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
 ]

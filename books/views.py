5f7c960906061ccc9f9fccad9fb38402ccef7556
hendisantika <hendisantika@yahoo.co.id>
2019-02-17 20:15:27 +0700

    #9 - Modify views.py file

--- books/views.py
+++ books/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import render
+
+from .models import Book
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})

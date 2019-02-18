9ffa1249e7acad7f4c429cea2890bc26538d6384
hendisantika <hendisantika@yahoo.co.id>
2019-02-18 19:50:11 +0700

    #19 - Add new route in urls.py file

--- django_simple_ajax_crud/urls.py
+++ django_simple_ajax_crud/urls.py
@@ -21,7 +21,7 @@ from books import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^books/$', views.book_list, name='book_list'),
-    # url(r'^books/create/$', views.book_create, name='book_create'),
+    url(r'^books/create/$', views.book_create, name='book_create'),
     # url(r'^books/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
     # url(r'^books/(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
 ]

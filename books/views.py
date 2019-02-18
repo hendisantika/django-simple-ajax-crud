5b40ee4a4e1bef3974482d1c2ff6546f1f101b22
hendisantika <hendisantika@yahoo.co.id>
2019-02-18 20:03:11 +0700

    #23 - Modify views.py file

--- books/views.py
+++ books/views.py
@@ -6,10 +6,21 @@ from .forms import BookForm
 
 
 def book_create(request):
-    form = BookForm()
+    data = dict()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+        else:
+            data['form_is_valid'] = False
+    else:
+        form = BookForm()
+
     context = {'form': form}
-    html_form = render_to_string('books/includes/partial_book_create.html',
-                                 context,
-                                 request=request,
-                                 )
-    return JsonResponse({'html_form': html_form})
+    data['html_form'] = render_to_string('books/includes/partial_book_create.html',
+                                         context,
+                                         request=request
+                                         )
+    return JsonResponse(data)

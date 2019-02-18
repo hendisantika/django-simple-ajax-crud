afac112f06a2a280a983c7268b149880fe31354a
hendisantika <hendisantika@yahoo.co.id>
2019-02-18 20:07:49 +0700

    #27 - Modify urls.py file - Edit Data

--- django_simple_ajax_crud/urls.py
+++ django_simple_ajax_crud/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^books/$', views.book_list, name='book_list'),
     url(r'^books/create/$', views.book_create, name='book_create'),
-    # url(r'^books/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
+    url(r'^books/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
     # url(r'^books/(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
 ]

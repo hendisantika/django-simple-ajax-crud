481d0e93982586846d156addaf1492639e499d79
hendisantika <hendisantika@yahoo.co.id>
2019-02-18 20:08:46 +0700

    #28 - Modify views.py file - Edit Data

--- books/views.py
+++ books/views.py
@@ -1,15 +1,15 @@
 # Create your views here.
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 
 from .forms import BookForm
+from .models import Book
 
 
-def book_create(request):
+def save_book_form(request, form, template_name):
     data = dict()
-
     if request.method == 'POST':
-        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             data['form_is_valid'] = True
@@ -19,12 +19,23 @@ def book_create(request):
             })
         else:
             data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
+
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
     else:
         form = BookForm()
+    return save_book_form(request, form, 'books/includes/partial_book_create.html')
 
-    context = {'form': form}
-    data['html_form'] = render_to_string('books/includes/partial_book_create.html',
-                                         context,
-                                         request=request
-                                         )
-    return JsonResponse(data)
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+    else:
+        form = BookForm(instance=book)
+    return save_book_form(request, form, 'books/includes/partial_book_update.html')
